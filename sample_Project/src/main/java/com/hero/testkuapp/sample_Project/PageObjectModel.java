beba4fc1b94e3ccfd5f36ff02ae2888bc17614b3
likitakotian <likitakotian123@gmail.com>
2022-12-09 21:32:21 +0530

    added 2 new test cases.

--- sample_Project/src/main/java/com/hero/testkuapp/sample_Project/PageObjectModel.java
+++ sample_Project/src/main/java/com/hero/testkuapp/sample_Project/PageObjectModel.java
@@ -1,8 +1,33 @@
 package com.hero.testkuapp.sample_Project;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class PageObjectModel {
 
+	 public WebDriver driver;
+
+	 public PageObjectModel(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	private By addElementButtonRoot = By.xpath("//button[contains(.,'Add Element')]");
+	private By deleteButtonRoot = By.xpath("//button[@class='added-manually']");
+	private By findImages = By.tagName("img");
+
+	public WebElement getButton() {
+		return driver.findElement(addElementButtonRoot);
+	}
 	
-	//new line added
+	public WebElement getDeleteButton() {
+		return driver.findElement(deleteButtonRoot);
+	}
 	
+	public List<WebElement> getImages() {
+		return driver.findElements(findImages);
+	}
+
 }
---
+++ sample_Project/src/test/java/com/hero/testkuapp/sample_Project/ChromeInitiationTest.java
@@ -0,0 +1,81 @@
+package com.hero.testkuapp.sample_Project;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Iterator;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import com.hero.testkuapp.sample_Project.PageObjectModel;
+
+@Test
+public class ChromeInitiationTest extends ConfigFile {
+
+	private WebDriver driver;
+	private PageObjectModel pom;
+	
+
+	@BeforeTest
+	public void initDriver() throws Exception {
+		initializeWebDriver();
+		driver = getInitializedDriver();
+		driver.get("https://the-internet.herokuapp.com/");
+		pom = new PageObjectModel(driver);
+	}
+
+	@Test(enabled = false)
+	public void clickOnElement() {
+		driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+		pom.getButton().click();
+		pom.getDeleteButton().click();
+	}
+
+	@Test(enabled = false)
+	public void valiadteAuthentication(String admin) {
+		String url = "https://admin:admin@the-internet.herokuapp.com/basic_auth";
+		driver.get(url);
+		driver.switchTo().alert().accept();
+	}
+
+	@Test(enabled = true)
+	public void findBrokenImages() {
+		// getattribute("src') URl
+		// url.oprnconnection()
+		// setrequestmethod(get)
+		// getresponsecode()
+
+		driver.get("https://the-internet.herokuapp.com/broken_images");
+		int invalidImageCount = 0;
+
+		for (WebElement imgElement : pom.getImages()) {
+			if (imgElement != null) {
+				try {
+					URL imageUrl = new URL(imgElement.getAttribute("src"));
+					HttpURLConnection httpURLConnection = (HttpURLConnection) imageUrl.openConnection();
+					httpURLConnection.setRequestMethod("GET");
+					int responseCode = httpURLConnection.getResponseCode();
+					if (responseCode != 200)
+						invalidImageCount++;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println("Total no. of invalid images are " + invalidImageCount);
+
+	}
+
+	@AfterTest
+	public void quitBrowser() {
+		driver.quit();
+	}
+}
