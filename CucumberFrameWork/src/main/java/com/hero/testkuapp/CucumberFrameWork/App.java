58e70e996454f150dc7b133a266eda5264b8f671
likitakotian <likitakotian123@gmail.com>
2022-12-13 13:36:30 +0530

    new project created

---
+++ CucumberFrameWork/src/main/java/com/hero/testkuapp/CucumberFrameWork/App.java
@@ -0,0 +1,13 @@
+package com.hero.testkuapp.CucumberFrameWork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
